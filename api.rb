17bcb40c0ac06b465f6856f22bafb94144702319
Franz Torghele <f.torghele@gmail.com>
2011-12-29 16:05:01 +0100

    simple RESTful API

--- api.rb
+++ api.rb
@@ -17,6 +17,14 @@ class Article
 
   #property :created_at, DateTime
   #property :updated_at, DateTime
+
+  def self.find_next(id)
+    Article.first(:id.gt =>  id)
+  end
+
+  def self.find_prev(id)
+    Article.last(:id.lt =>  id)
+  end
 end
 
 #Setup Database
@@ -29,35 +37,70 @@ class ArticleApi < Sinatra::Base
   use ResponseTimer
 
   helpers do
+    def api_links(action = nil, id = nil)
+      case action
+        when :entryPoint
+          [ { :link => { :rel => "all", :uri => "/articles/"} },
+            { :link => { :rel => "new", :uri => "/articles/"} }]
+        when :allArticles
+          [ { :link => { :rel => "new", :uri => "/articles/"} } ]
+        when :article
+          next_record = Article.find_next(id)
+          prev_record = Article.find_prev(id)
+          [ { :link => { :rel => "self", :uri => "/articles/#{id}"} },
+            { :link => { :rel => "update", :uri => "/articles/#{id}"} },
+            { :link => { :rel => "delete", :uri => "/articles/#{id}"} },
+            { :link => { :rel => "next", :uri => next_record.nil? ? nil : "/articles/#{next_record.id}"} },
+            { :link => { :rel => "prev", :uri => prev_record.nil? ? nil : "/articles/#{prev_record.id}"} }]
+        when :errorResponse
+          [ { :link => { :rel => "all", :uri => "/articles/"} },
+            { :link => { :rel => "new", :uri => "/articles/"} }]
+      end
+    end
+
     def return_json_status(code, msg)
       status code
       {
         :status => code,
-        :reason => msg
+        :reason => msg,
+        :api => api_links(:errorResponse)
       }.to_json
     end
   end
 
-  # GET /article - return all articles
-  get "/articles/?", :provides => :json do
+  # GET / - entry point
+  get "/", :provides => :json do
     content_type :json
-
-    Article.all.to_json
+    {
+      :api => api_links(:entryPoint)
+    }.to_json
   end
 
-  ## GET /article/:id - return article with specified id
+  # GET /articles - return all articles
+  get "/articles/?", :provides => :json do
+    content_type :json
+    {
+      :content => Article.all.collect{ |a| { :title => a.title, :text => a.text, :api => api_links(:article, a.id)} },
+      :api => api_links(:allArticles)
+    }.to_json
+  end
+
+  ## GET /articles/:id - return article with specified id
   get "/articles/:id", :provides => :json do
     content_type :json
 
     if article = Article.first(:id => params[:id].to_i)
-      article.to_json
+      {
+        :content => article,
+        :api => api_links(:article, article.id)
+      }.to_json
     else
       return_json_status 404, "Not found"
     end
 
   end
 
-  ## POST /article/ - create new article
+  ## POST /articles/ - create new article
   post "/articles/?", :provides => :json do
     content_type :json
 
@@ -65,13 +108,16 @@ class ArticleApi < Sinatra::Base
     if article.save
       headers["Location"] = "/article/#{article.id}"
       status 201 # Created
-      article.to_json
+      {
+        :content => article,
+        :api => api_links(:article, article.id)
+      }.to_json
     else
       return_json_status 400, article.errors.to_hash
     end
   end
 
-  # PUT /article/:id - change a whole article
+  # PUT /articles/:id - change a whole article
   put "/articles/:id", :provides => :json do
     content_type :json
 
@@ -81,7 +127,10 @@ class ArticleApi < Sinatra::Base
       article.text = params[:text] unless params[:text].nil?
 
       if article.save
-        article.to_json
+        {
+          :content => article,
+          :api => api_links(:article, article.id)
+        }.to_json
       else
         return_json_status 400, article.errors.to_hash
       end
@@ -90,7 +139,7 @@ class ArticleApi < Sinatra::Base
     end
   end
 
-  # DELETE /article/:id - delete a article
+  # DELETE /articles/:id - delete a article
   delete "/articles/:id/?", :provides => :json do
     content_type :json
 
@@ -101,4 +150,10 @@ class ArticleApi < Sinatra::Base
       return_json_status 404, "Not found"
     end
   end
+
+  # Handle wrong requests
+  get "*" do status 404 end
+  post "*" do status 404 end
+  put "*" do status 404 end
+  delete "*" do status 404 end
 end
